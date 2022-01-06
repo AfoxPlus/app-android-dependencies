0c56e4a87f68b3e8dd304251ea31c908b4b1f8f2
Valentin Mendoza Torres <jmendozat13@gmail.com>
2022-01-05 22:44:43 -0500

    Develop (#4)
    * Update dependencies.kt
    
    * sea agrego la libreria de zxing (#3)
    
    * sea agrego la libreria de zxing
    
    * Update dependencies.kt
    
    Co-authored-by: afoxpluss <92241438+afoxpluss@users.noreply.github.com>
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    Co-authored-by: afoxpluss <92241438+afoxpluss@users.noreply.github.com>
    Co-authored-by: Carlos Osorio <hacybeyker@gmail.com>

--- dependencies.kt
+++ dependencies.kt
@@ -27,8 +27,10 @@ object Versions {
 	
     //Business Modules
     const val uikitVersion                      = "2.0.0"
-    const val productsVersion                   = "1.0.1-feature-offer-SNAPSHOT"
+    const val networkVersion                    = "1.0.0"
+    const val productsVersion                   = "1.0.0"
     const val homeVersion                       = "1.0.0"
+    const val restaurantsVersion                = "1.0.0"
 
     //Views
     const val materialVersion                   = "1.4.0"
@@ -61,7 +63,7 @@ object Deps {
         const val coroutinesCore                = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutinesVersion}"
         const val coroutinesAndroid             = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesVersion}"
         const val coreHiltAndroid               = "com.google.dagger:hilt-core:${Versions.hiltVersion}"
-	    const val hiltAndroid                   = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
+	      const val hiltAndroid                   = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
         const val hiltCompiler                  = "com.google.dagger:hilt-compiler:${Versions.hiltVersion}"
         const val retrofit2                     = "com.squareup.retrofit2:retrofit:${Versions.retrofit2Version}"
         const val gson                          = "com.squareup.retrofit2:converter-gson:${Versions.retrofit2Version}"
@@ -72,8 +74,11 @@ object Deps {
         const val lifecycleViewModel            = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleVersion}"
         const val lifecycleLiveData             = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleVersion}"
         const val lifecycleRuntime              = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleVersion}"
-        const val products               	    = "com.afoxplus.android:products:${Versions.productsVersion}"
+        
+        const val products               	      = "com.afoxplus.android:products:${Versions.productsVersion}"
+	      const val network               	      = "com.afoxplus.android:network:${Versions.networkVersion}"
         const val home                         	= "com.afoxplus.android:home:${Versions.homeVersion}"
+        const val restaurants                   = "com.afoxplus.android:restaurants:${Versions.restaurantsVersion}"
 			
         const val chucker                       = "com.github.chuckerteam.chucker:library:${Versions.chuckerVersion}"
         const val chuckerNoOp                   = "com.github.chuckerteam.chucker:library-no-op:${Versions.chuckerVersion}"
