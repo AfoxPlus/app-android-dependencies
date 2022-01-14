fb3c4c9663e9df4f9e67a74043c4ef0a81f2a2c7
Valentin Mendoza Torres <jmendozat13@gmail.com>
2022-01-13 23:45:18 -0500

    Develop (#5)
    * Update dependencies
    
    * Update develop (#2)
    
    * Update dependencies
    
    * Update dependencies
    
    * Update versions
    
    Co-authored-by: afoxpluss <92241438+afoxpluss@users.noreply.github.com>
    
    * sea agrego la libreria de zxing
    
    * se modificaron las versiones de los modulos de negocio
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * sea agrego la libreria de zxing (#3)
    
    * sea agrego la libreria de zxing
    
    * Update dependencies.kt
    
    Co-authored-by: afoxpluss <92241438+afoxpluss@users.noreply.github.com>
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    Co-authored-by: afoxpluss <92241438+afoxpluss@users.noreply.github.com>
    Co-authored-by: Carlos Osorio <hacybeyker@gmail.com>

--- dependencies.kt
+++ dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
     const val targetSdkVersion                  = 30
 
     const val testInstrumentationRunner         = "androidx.test.runner.AndroidJUnitRunner"
-	
+
     //Android
     const val coreVersion                       = "1.6.0"
     const val appcompatVersion                  = "1.3.1"
@@ -24,23 +24,33 @@ object Versions {
     const val roomVersion                       = "2.3.0"
     const val lifecycleVersion                  = "2.3.1"
     const val chuckerVersion                    = "3.5.0"
-	
+
     //Business Modules
-    const val uikitVersion                      = "2.0.0"
+    const val uikitVersion                      = "2.1.0"
+    const val networkVersion                    = "1.0.0"
     const val productsVersion                   = "1.0.0"
-    //const val OrdersCoreVersion               = "1.0.0-SNAPSHOT"
+    const val homeVersion                       = "1.0.0"
+    const val restaurantsVersion                = "1.0.0"
 
     //Views
     const val materialVersion                   = "1.4.0"
     const val recyclerviewVersion               = "1.2.1"
     const val constraintLayoutVersion           = "2.1.0"
     const val glideVersion                      = "4.12.0"
-	
+
     //Test
     const val jUnitVersion                      = "4.13.2"
     const val androidJUnitVersion               = "1.1.3"
     const val espressoVersion                   = "3.4.0"
-	
+    const val testCoreVersion                   = "2.1.0"
+    const val truthVersion                      = "1.1.3"
+    const val mockitoVersion                    = "2.28.2"
+    const val mockitoKotlinVersion              = "4.0.0"
+
+    //Scan
+    const val zxingAndroidVersion               = "4.2.0"
+    const val zxingCoreVersion                  = "3.4.0"
+
 }
 
 object Deps {
@@ -57,7 +67,7 @@ object Deps {
         const val coroutinesCore                = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutinesVersion}"
         const val coroutinesAndroid             = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesVersion}"
         const val coreHiltAndroid               = "com.google.dagger:hilt-core:${Versions.hiltVersion}"
-	const val hiltAndroid                   = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
+	      const val hiltAndroid                   = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
         const val hiltCompiler                  = "com.google.dagger:hilt-compiler:${Versions.hiltVersion}"
         const val retrofit2                     = "com.squareup.retrofit2:retrofit:${Versions.retrofit2Version}"
         const val gson                          = "com.squareup.retrofit2:converter-gson:${Versions.retrofit2Version}"
@@ -67,13 +77,17 @@ object Deps {
         const val roomCompiler                  = "androidx.room:room-compiler:${Versions.roomVersion}"
         const val lifecycleViewModel            = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleVersion}"
         const val lifecycleLiveData             = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleVersion}"
-        const val lifecycleRuntime              = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleVersion}"
-        const val products               	= "com.afoxplus.android:products:${Versions.productsVersion}"
-	//const val variantOrdersCore           = "com.github.AfoxPlus:variant_orders_core:${Versions.variantOrdersCore}"
-        
-			
+        const val lifecycleRuntime              = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleVersion}"    
         const val chucker                       = "com.github.chuckerteam.chucker:library:${Versions.chuckerVersion}"
         const val chuckerNoOp                   = "com.github.chuckerteam.chucker:library-no-op:${Versions.chuckerVersion}"
+        const val zxingAndroid                  = "com.journeyapps:zxing-android-embedded:${Versions.zxingAndroidVersion}"
+        const val zxingCore                     = "com.google.zxing:core:${Versions.zxingCoreVersion}"
+	    
+        //Business Modules
+        const val products               	      = "com.afoxplus.android:products:${Versions.productsVersion}"
+        const val network               	      = "com.afoxplus.android:network:${Versions.networkVersion}"
+        const val home                         	= "com.afoxplus.android:home:${Versions.homeVersion}"
+        const val restaurants                   = "com.afoxplus.android:restaurants:${Versions.restaurantsVersion}"
     }
 
     object UI {
@@ -89,5 +103,9 @@ object Deps {
         const val jUnit                         = "junit:junit:${Versions.jUnitVersion}"
         const val androidJUnit                  = "androidx.test.ext:junit:${Versions.androidJUnitVersion}"
         const val espresso                      = "androidx.test.espresso:espresso-core:${Versions.espressoVersion}"
+        const val testCore                      = "androidx.arch.core:core-testing:${Versions.testCoreVersion}"
+        const val truth                         = "com.google.truth:truth:${Versions.truthVersion}"
+        const val mockito                       = "org.mockito:mockito-core:${Versions.mockitoVersion}"
+        const val mockitoKotlin                 = "org.mockito.kotlin:mockito-kotlin:${Versions.mockitoKotlinVersion}"
     }
 }
