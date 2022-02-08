5f4aa70aeae1cec0969c8a1b980c6d0a486de7d2
Valentin Mendoza Torres <jmendozat13@gmail.com>
2022-02-07 21:24:46 -0500

    Update dependencies.kt

--- dependencies.kt
+++ dependencies.kt
@@ -28,7 +28,7 @@ object Versions {
     //Business Modules
     const val uikitVersion                      = "2.1.1"
     const val networkVersion                    = "1.0.0"
-    const val productsVersion                   = "1.2.0-beta"
+    const val productsVersion                   = "2.0.0"
     const val homeVersion                       = "1.0.1"
     const val restaurantsVersion                = "1.0.0"
     const val ordersVersion                     = "1.0.0"
