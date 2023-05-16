294df48dd67440b45dfd48e3439437781d1558f3
Carlos Osorio <hacybeyker@gmail.com>
2023-05-15 19:18:47 -0500

    Rc01.2022 (#9)
    * Update dependencies
    
    * Update develop (#2)
    
    * Update dependencies
    
    * Update dependencies
    
    * Update versions
    
    Co-authored-by: afoxpluss <92241438+afoxpluss@users.noreply.github.com>
    
    * sea agrego la libreria de zxing
    
    * se modificaron las versiones de los modulos de negocio
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * sea agrego la libreria de zxing (#3)
    
    * sea agrego la libreria de zxing
    
    * Update dependencies.kt
    
    Co-authored-by: afoxpluss <92241438+afoxpluss@users.noreply.github.com>
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt
    
    * Update dependencies.kt (#6)
    
    * Dependencies (#7)
    
    * Update dependencies.kt
    
    * se agregaron dependencias de test
    
    Co-authored-by: afoxpluss <92241438+afoxpluss@users.noreply.github.com>
    
    * update uikit version to 2.1.3-RC01-2022-SNAPSHOT
    
    * Change the compile sdk version to 31
    
    * Update dependencies.kt
    
    * updated order version to 1.0.2-RC01.2022-SNAPSHOT
    
    * update uikit version to 2.1.4-RC01.2022-SNAPSHOT
    
    * update orders version to 1.0.3-RC01.2022-SNAPSHOT
    
    * Added shimmer dependencies
    
    * se modifico la version de uikit
    
    * mejoras en versiones
    
    * se modifico la version de homeVersion
    
    * update version UIKIT 2.3.1-RC01.2022-SNAPSHOT
    
    * update orders version to 2.0.0-RC01.2022-SNAPSHOT
    
    * update the uikit version to 3.0.1-RC01.2022-SNAPSHOT
    
    * update order version to 2.1.0-RC01.2022-SNAPSHOT
    
    * Feature update dependencies (#8)
    
    * update dependencies
    
    * se actualizaron las dependencias
    
    ---------
    
    Co-authored-by: Carlos Osorio <hacybeyker@MacBook-Pro-de-Carlos.local>
    
    ---------
    
    Co-authored-by: Valentin Mendoza Torres <jmendozat13@gmail.com>
    Co-authored-by: afoxpluss <92241438+afoxpluss@users.noreply.github.com>
    Co-authored-by: Petter Jhunior Garcia Infante <petterjunior94@gmail.com>
    Co-authored-by: Petter Jhunior Garcia Infante <42043134+JhPetter@users.noreply.github.com>
    Co-authored-by: Petter Garcia <petter.infante@macys.com>
    Co-authored-by: Carlos Osorio <hacybeyker@MacBook-Pro-de-Carlos.local>

--- dependencies.kt
+++ dependencies.kt
@@ -1,42 +1,44 @@
 object Versions {
     //Kotlin
-    const val kotlinVersion                     = "1.5.21"
+    const val kotlinVersion                     = "1.8.20"
     //SDK
-    const val compileSdkVersion                 = 30
-    const val buildToolsVersion                 = "30.0.3"
+    const val compileSdkVersion                 = 33
+    const val buildToolsVersion                 = "31.0.0"
     const val minSdkVersion                     = 23
-    const val targetSdkVersion                  = 30
+    const val targetSdkVersion                  = 33
 
     const val testInstrumentationRunner         = "androidx.test.runner.AndroidJUnitRunner"
 
     //Android
-    const val coreVersion                       = "1.6.0"
-    const val appcompatVersion                  = "1.3.1"
-    const val activityVersion                   = "1.3.1"
-    const val fragmentVersion                   = "1.3.6"
+    const val coreVersion                       = "1.10.1"
+    const val appcompatVersion                  = "1.6.1"
+    const val activityVersion                   = "1.6.1"
+    const val fragmentVersion                   = "1.5.5"
 
     //Architecture
-    const val hiltVersion                       = "2.38.1"
-    const val coroutinesVersion                 = "1.5.0"
+    const val hiltVersion                       = "2.46"
+    const val coroutinesVersion                 = "1.7.0"
     const val retrofit2Version                  = "2.9.0"
-    const val okhttp3Version                    = "4.9.1"
-    const val okHttpJsonMockVersion             = "3.0"
-    const val roomVersion                       = "2.3.0"
-    const val lifecycleVersion                  = "2.3.1"
+    const val okhttp3Version                    = "4.11.0"
+    const val okHttpJsonMockVersion             = "1.1.1"
+    const val roomVersion                       = "2.5.1"
+    const val lifecycleVersion                  = "2.6.1"
     const val chuckerVersion                    = "3.5.0"
 
     //Business Modules
-    const val uikitVersion                      = "2.1.0"
+    const val uikitVersion                      = "5.1.0"
     const val networkVersion                    = "1.0.0"
-    const val productsVersion                   = "1.0.0"
-    const val homeVersion                       = "1.0.0"
-    const val restaurantsVersion                = "1.0.0"
+    const val productsVersion                   = "2.0.1-RC01.2022-SNAPSHOT"
+    const val homeVersion                       = "1.1.0-RC01.2022-SNAPSHOT"
+    const val restaurantsVersion                = "1.1.0-RC01.2021-SNAPSHOT"
+    const val ordersVersion                     = "2.1.0-RC01.2022-SNAPSHOT"
 
     //Views
-    const val materialVersion                   = "1.4.0"
+    const val materialVersion                   = "1.9.0"
     const val recyclerviewVersion               = "1.2.1"
-    const val constraintLayoutVersion           = "2.1.0"
-    const val glideVersion                      = "4.12.0"
+    const val constraintLayoutVersion           = "2.1.4"
+    const val glideVersion                      = "4.15.1"
+    const val fbShimmerVersion                  = "0.5.0"
 
     //Test
     const val jUnitVersion                      = "4.13.2"
@@ -46,6 +48,8 @@ object Versions {
     const val truthVersion                      = "1.1.3"
     const val mockitoVersion                    = "2.28.2"
     const val mockitoKotlinVersion              = "4.0.0"
+    const val mockitoInlineVersion              = "4.3.1"
+    const val kotlinCoroutineVersion            = "1.6.0"
 
     //Scan
     const val zxingAndroidVersion               = "4.2.0"
@@ -67,7 +71,7 @@ object Deps {
         const val coroutinesCore                = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutinesVersion}"
         const val coroutinesAndroid             = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesVersion}"
         const val coreHiltAndroid               = "com.google.dagger:hilt-core:${Versions.hiltVersion}"
-	      const val hiltAndroid                   = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
+	    const val hiltAndroid                   = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
         const val hiltCompiler                  = "com.google.dagger:hilt-compiler:${Versions.hiltVersion}"
         const val retrofit2                     = "com.squareup.retrofit2:retrofit:${Versions.retrofit2Version}"
         const val gson                          = "com.squareup.retrofit2:converter-gson:${Versions.retrofit2Version}"
@@ -77,17 +81,18 @@ object Deps {
         const val roomCompiler                  = "androidx.room:room-compiler:${Versions.roomVersion}"
         const val lifecycleViewModel            = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleVersion}"
         const val lifecycleLiveData             = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleVersion}"
-        const val lifecycleRuntime              = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleVersion}"    
+        const val lifecycleRuntime              = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleVersion}"
         const val chucker                       = "com.github.chuckerteam.chucker:library:${Versions.chuckerVersion}"
         const val chuckerNoOp                   = "com.github.chuckerteam.chucker:library-no-op:${Versions.chuckerVersion}"
         const val zxingAndroid                  = "com.journeyapps:zxing-android-embedded:${Versions.zxingAndroidVersion}"
         const val zxingCore                     = "com.google.zxing:core:${Versions.zxingCoreVersion}"
-	    
+
         //Business Modules
-        const val products               	      = "com.afoxplus.android:products:${Versions.productsVersion}"
-        const val network               	      = "com.afoxplus.android:network:${Versions.networkVersion}"
+        const val products               	    = "com.afoxplus.android:products:${Versions.productsVersion}"
+        const val network               	    = "com.afoxplus.android:network:${Versions.networkVersion}"
         const val home                         	= "com.afoxplus.android:home:${Versions.homeVersion}"
         const val restaurants                   = "com.afoxplus.android:restaurants:${Versions.restaurantsVersion}"
+	    const val orders                        = "com.afoxplus.android:orders:${Versions.ordersVersion}"
     }
 
     object UI {
@@ -97,6 +102,7 @@ object Deps {
         const val recyclerview                  = "androidx.recyclerview:recyclerview:${Versions.recyclerviewVersion}"
         const val glide                         = "com.github.bumptech.glide:glide:${Versions.glideVersion}"
         const val glideCompiler                 = "com.github.bumptech.glide:compiler:${Versions.glideVersion}"
+        const val fbShimmer                     = "com.facebook.shimmer:shimmer:${Versions.fbShimmerVersion}"
     }
 
     object Test {
@@ -107,5 +113,7 @@ object Deps {
         const val truth                         = "com.google.truth:truth:${Versions.truthVersion}"
         const val mockito                       = "org.mockito:mockito-core:${Versions.mockitoVersion}"
         const val mockitoKotlin                 = "org.mockito.kotlin:mockito-kotlin:${Versions.mockitoKotlinVersion}"
+        const val mockitoInline                 = "org.mockito:mockito-inline:${Versions.mockitoInlineVersion}"
+        const val kotlinCoroutine               = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.kotlinCoroutineVersion}"
     }
 }
