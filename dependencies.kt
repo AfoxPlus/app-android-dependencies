1252f018082c68282fa2b697e14eb41f9e3ae823
Valentin Mendoza Torres <jmendozat13@gmail.com>
2023-06-04 13:02:40 -0500

    Update product version

--- dependencies.kt
+++ dependencies.kt
@@ -28,7 +28,7 @@ object Versions {
     //Business Modules
     const val uikitVersion                      = "5.3.0"
     const val networkVersion                    = "1.2.0"
-    const val productsVersion                   = "3.0.1-update_apis-SNAPSHOT"
+    const val productsVersion                   = "3.0.2"
     const val homeVersion                       = "3.0.1-update_api-SNAPSHOT"
     const val restaurantsVersion                = "3.0.1-update_api-SNAPSHOT"
     const val ordersVersion                     = "3.0.1-update_api-SNAPSHOT"
