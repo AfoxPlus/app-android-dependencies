715849ac336de90febcf0f4dc358b8e8ac4f1bec
Valentin Mendoza Torres <jmendozat13@gmail.com>
2023-07-18 23:37:39 -0500

    Update dependencies.kt

--- dependencies.kt
+++ dependencies.kt
@@ -33,7 +33,7 @@ object Versions {
     const val restaurantsVersion                = "3.0.4"
     const val ordersVersion                     = "3.0.12"
     const val authVersion                       = "1.0.0"
-    const val demo_config                     	= "1.0.0"
+    const val demo_config                     	= "1.0.1"
 
     //Views
     const val materialVersion                   = "1.9.0"
