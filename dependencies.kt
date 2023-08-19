6955624883bcceeb0a96e62ed186070a38f5c21e
Petter Infante <petter.infante@macys.com>
2023-08-19 15:30:44 -0500

    update restaurantVersion to 3.1.2

--- dependencies.kt
+++ dependencies.kt
@@ -30,7 +30,7 @@ object Versions {
     const val networkVersion                    = "1.2.4"
     const val productsVersion                   = "3.1.1"
     const val homeVersion                       = "3.1.3"
-    const val restaurantsVersion                = "3.1.1"
+    const val restaurantsVersion                = "3.1.2"
     const val ordersVersion                     = "3.1.5"
     const val authVersion                       = "1.0.0"
     const val demo_config                     	= "1.0.1"
