ae378eaaa41c8ac00e1bb705d98c1ebe262df351
Petter Infante <petter.infante@macys.com>
2023-08-27 13:59:05 -0500

    update the orders version to 3.1.9

--- dependencies.kt
+++ dependencies.kt
@@ -31,7 +31,7 @@ object Versions {
     const val productsVersion                   = "3.1.2"
     const val homeVersion                       = "3.1.6"
     const val restaurantsVersion                = "3.1.2"
-    const val ordersVersion                     = "3.1.8"
+    const val ordersVersion                     = "3.1.9"
     const val authVersion                       = "1.0.0"
     const val demo_config                     	= "1.0.1"
 
