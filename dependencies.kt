fbdd869af276762a016f1be9c7d08e10453d0cba
Carlos Osorio <hacybeyker@gmail.com>
2023-10-27 17:50:38 -0500

    Rc02.2023 (#16)
    * Update product version
    
    * update orders version
    
    * Update home version
    
    * update the orders version to 3.1.9
    
    * change the uikitversion to 5.3.11
    
    * change order version to 5.3.11
    
    * Update orders version
    
    * update versions
    
    ---------
    
    Co-authored-by: Valentin Mendoza Torres <jmendozat13@gmail.com>
    Co-authored-by: Petter Infante <petter.infante@macys.com>

--- dependencies.kt
+++ dependencies.kt
@@ -26,14 +26,13 @@ object Versions {
     const val chuckerVersion                    = "3.5.2"
 
     //Business Modules
-    const val uikitVersion                      = "5.3.10"
-    const val networkVersion                    = "1.2.5"
-    const val productsVersion                   = "3.1.1"
-    const val homeVersion                       = "3.1.5"
-    const val restaurantsVersion                = "3.1.2"
-    const val ordersVersion                     = "3.1.7"
-    const val authVersion                       = "1.0.0"
-    const val demo_config                     	= "1.0.1"
+    const val uikitVersion                      = "6.0.0"
+    const val networkVersion                    = "6.0.0"
+    const val productsVersion                   = "6.0.0"
+    const val homeVersion                       = "6.0.0"
+    const val restaurantsVersion                = "6.0.0"
+    const val ordersVersion                     = "6.0.0"
+    const val demo_config                     	= "6.0.0"
 
     //Views
     const val materialVersion                   = "1.9.0"
@@ -73,7 +72,7 @@ object Deps {
         const val coroutinesCore                = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutinesVersion}"
         const val coroutinesAndroid             = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesVersion}"
         const val coreHiltAndroid               = "com.google.dagger:hilt-core:${Versions.hiltVersion}"
-	const val hiltAndroid                   = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
+	    const val hiltAndroid                   = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
         const val hiltCompiler                  = "com.google.dagger:hilt-compiler:${Versions.hiltVersion}"
         const val retrofit2                     = "com.squareup.retrofit2:retrofit:${Versions.retrofit2Version}"
         const val gson                          = "com.squareup.retrofit2:converter-gson:${Versions.retrofit2Version}"
@@ -90,12 +89,11 @@ object Deps {
         const val zxingCore                     = "com.google.zxing:core:${Versions.zxingCoreVersion}"
 
         //Business Modules
-        const val products               	= "com.afoxplus.android:products:${Versions.productsVersion}"
-        const val network               	= "com.afoxplus.android:network:${Versions.networkVersion}"
+        const val products               	    = "com.afoxplus.android:products:${Versions.productsVersion}"
+        const val network               	    = "com.afoxplus.android:network:${Versions.networkVersion}"
         const val home                         	= "com.afoxplus.android:home:${Versions.homeVersion}"
         const val restaurants                   = "com.afoxplus.android:restaurants:${Versions.restaurantsVersion}"
-	const val orders                        = "com.afoxplus.android:orders:${Versions.ordersVersion}"
-        const val auth                          = "com.afoxplus.android:auth:${Versions.authVersion}"
+	    const val orders                        = "com.afoxplus.android:orders:${Versions.ordersVersion}"
         const val demo_config                   = "com.afoxplus.android:demo_config:${Versions.demo_config}"
     }
 
