804be6b415b043e7a55239d17b278f51791ed125
Valentin Mendoza Torres <jmendozat13@gmail.com>
2024-08-13 14:26:32 -0500

    update core version

--- dependencies.kt
+++ dependencies.kt
@@ -40,8 +40,8 @@ object Versions {
     const val bomFirebase                       = "32.6.0"
 
     //Business Modules
-    const val uikitVersion                      = "6.1.1"
-    const val networkVersion                    = "6.1.1"
+    const val uikitVersion                      = "6.2.0"
+    const val networkVersion                    = "6.2.0"
     const val productsVersion                   = "6.1.0"
     const val homeVersion                       = "6.2.0"
     const val restaurantsVersion                = "6.1.1"
