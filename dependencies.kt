2ac353dc248c756a51ab29e0db55e7509e97bf05
afoxpluss <92241438+afoxpluss@users.noreply.github.com>
2024-08-14 19:58:27 -0500

    Update dependencies.kt

--- dependencies.kt
+++ dependencies.kt
@@ -43,7 +43,7 @@ object Versions {
     const val uikitVersion                      = "6.2.0"
     const val networkVersion                    = "6.2.0"
     const val productsVersion                   = "6.2.0"
-    const val homeVersion                       = "6.3.0"
+    const val homeVersion                       = "6.3.1"
     const val restaurantsVersion                = "6.2.0"
     const val ordersVersion                     = "6.2.0"
     const val invitationVersion                 = "1.3.0"
