93261ac4a274b7a7826e32bdc1b15f3674d057e1
Valentin Mendoza Torres <jmendozat13@gmail.com>
2025-01-26 21:41:05 -0500

    Update dependencies.kt

--- dependencies.kt
+++ dependencies.kt
@@ -42,7 +42,7 @@ object Versions {
     const val bomFirebase = "32.6.0"
 
     //Business Modules
-    const val uikitVersion = "6.3.0"
+    const val uikitVersion = "6.3.1"
     const val networkVersion = "6.3.0"
     const val productsVersion = "6.2.2"
     const val homeVersion = "6.3.5"
